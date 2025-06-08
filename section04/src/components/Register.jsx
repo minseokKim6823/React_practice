ba89030c52f75ce725dfd3d712ba3226220fbe83
minseokKim6823 <alstjr6823@naver.com>
2025-06-08 17:05:31 +0900

    State

--- section04/src/components/Register.jsx
+++ section04/src/components/Register.jsx
@@ -1,53 +1,61 @@
 import {useState} from "react";
 
-//간단한 회원가입 폼
-//1. 이름
-//2. 생년월일
-//3. 국적
-//4. 자기소개
+// 간단한 회원가입 폼
+// 1. 이름
+// 2. 생년월일
+// 3. 국적
+// 4. 자기소개
 
 const Register= () => {
-    const [name, setName] = useState("");
-    const [birth, setBirth] = useState("");
-    const [country, setCountry] = useState("");
-    const [bio, setBio] = useState("");
+    const [input, setInput] = useState({
+        name: "",
+        birth: "",
+        country: "",
+        bio: "",
+    });
+
+    const onChange = (e) => {
+        console.log(e.target.name, e.target.value);
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value,
+        });
+    };
+
 
     const onChangeName= (e) => {
-        setName(e.target.value);
+        setInput({
+            ...input,
+            [e.target.name] : e.target.value
+        });
     };
-
-    const onChangeBirth= (e) => {
-        setBirth(e.target.value);
-    };
-
-    const onChangeCountry= (e) => {
-        setCountry(e.target.value);
-    };
-
-    const onChangeBio= (e) => {
-        setBio(e.target.value);
-    }
 
     return (
         <div>
             <div>
                 <input
-                    value ={name}
-                    onChange = {onChangeName}
+                    name = "name"
+                    value ={input.name}
+                    onChange = {onChange}
                     placeholder={"이름"}
                 />
             </div>
 
             <div>
                 <input
-                    value = {birth}
-                    onChange = {onChangeBirth}
+                    name = "birth"
+                    value = {input.birth}
+                    onChange = {onChange}
                     type = "date"
                 />
             </div>
 
             <div>
-                <select value = {country} onChange = {onChangeCountry}>
+                <select
+                    name = "country"
+                    value = {input.country}
+                    onChange = {onChange}
+                >
                     <option value = ""></option>
                     <option value = "KR">한국</option>
                     <option value = "US">미국</option>
@@ -56,9 +64,12 @@ const Register= () => {
             </div>
 
             <div>
-                <textarea  value = {bio} onChange = {onChangeBio}/>
+                <textarea
+                    name = "bio"
+                    value = {input.bio}
+                    onChange = {onChange}
+                />
             </div>
-            {bio}
         </div>
     );
 };
