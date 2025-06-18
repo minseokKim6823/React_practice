f1c8aff634cfee48ad1c4159e1eb293430b8f555
minseokKim6823 <alstjr6823@naver.com>
2025-06-18 23:44:47 +0900

    카운터 앱

--- section04/src/components/Register.jsx
+++ section04/src/components/Register.jsx
@@ -1,5 +1,4 @@
-import {useState} from "react";
-
+import {useState, useRef} from "react";
 // 간단한 회원가입 폼
 // 1. 이름
 // 2. 생년월일
@@ -13,9 +12,15 @@ const Register= () => {
         country: "",
         bio: "",
     });
+    const countRef = useRef(0);
+    const inputRef = useRef();
+
+    let count = 0;
 
     const onChange = (e) => {
-        console.log(e.target.name, e.target.value);
+        // countRef.current++;
+        count++;
+        console.log(countRef.current);
         setInput({
             ...input,
             [e.target.name]: e.target.value,
@@ -23,17 +28,18 @@ const Register= () => {
     };
 
 
-    const onChangeName= (e) => {
-        setInput({
-            ...input,
-            [e.target.name] : e.target.value
-        });
+    const onSubmit = () => {
+        if(input.name === "") {
+            //이름을 입력하는 DOM 요소 포커스
+            inputRef.current.focus();
+        }
     };
 
     return (
         <div>
             <div>
                 <input
+                    ref = {inputRef}
                     name = "name"
                     value ={input.name}
                     onChange = {onChange}
@@ -70,6 +76,10 @@ const Register= () => {
                     onChange = {onChange}
                 />
             </div>
+
+            <button onClick={onSubmit}>
+                제출
+            </button>
         </div>
     );
 };
